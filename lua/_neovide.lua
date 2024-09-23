708cb869fff55e58f8c6ed8ce6e48b323d4689e8
th3r00t <myoung0083@gmail.com>
2024-09-23 00:49:04 -0400

    Cleaned working directory. Prepared for import.

--- lua/_neovide.lua
+++
@@ -1,2 +0,0 @@
-local vim = vim
-vim.opt.guifont = "FiraCode Nerd Font Mono:h6" -- the font used in graphical neovim applications
--- lua/_snippets.lua
+++
@@ -1,4 +0,0 @@
-require("luasnip.loaders.from_vscode").lazy_load()
--- load snippets from path/of/your/nvim/config/my-cool-snippets
--- require("luasnip.loaders.from_vscode").lazy_load({ paths = { "lua/snippets" } })
-require("luasnip.loaders.from_snipmate").lazy_load()
